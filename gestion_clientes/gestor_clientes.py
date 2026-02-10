c2f24c4d9454fdd5d4589aefbf1ee95164f997e2
Auditor2003 <diegomunozlasanta@gmail.com>
2026-02-09 22:42:01 -0300

    Ok con diccionario (revisar como se almacena) + ingreso + Validación de clientes (Revisar) + Listar clientes (Me sale error-->Revisar)

--- gestion_clientes/gestor_clientes.py
+++ gestion_clientes/gestor_clientes.py
@@ -18,3 +18,50 @@ from clientes.cliente_corporativo import ClienteCorporativo
 
 ### No se han creado aun las clases ni las fubnciones ### 
 ### Sólo traigo esquema inicial de ABP 3 ###
+
+class GestorClientes:
+    """
+    Clase GestorClientes
+    Esta clase sólo maneja el sistema
+    """
+
+    def __init__(self):
+        
+        # Para crear el diccionario
+
+        self._clientes = {
+            "regular": [],
+            "premium": [],
+            "corporativo": []
+        }
+
+        # Aqui ya se agrega un cliente al sistema--->Ver donde se almacena !!!
+    def agregar_cliente(self, cliente):
+        
+        # Validamos que sea un objeto Cliente
+        if not isinstance(cliente, Cliente):
+            print("Error: el objeto (clase) no es un Cliente válido.")
+            return
+
+        # Determinamos el tipo de cliente
+        if isinstance(cliente, ClienteRegular):
+            self._clientes["regular"].append(cliente)
+
+        elif isinstance(cliente, ClientePremium):
+            self._clientes["premium"].append(cliente)
+
+        elif isinstance(cliente, ClienteCorporativo):
+            self._clientes["corporativo"].append(cliente)
+
+        else:
+            print("Tipo de cliente no reconocido.")
+            return
+
+        print("Cliente agregado correctamente.")
+
+    def listar_clientes(self):
+        """
+        Debería mostrar todos los clientes según su tipo
+        """
+
+        
