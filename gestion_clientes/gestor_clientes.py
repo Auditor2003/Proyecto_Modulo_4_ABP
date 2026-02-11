6480341c789c308fd429357c86fba0a07742adf0
Auditor2003 <diegomunozlasanta@gmail.com>
2026-02-11 18:03:41 -0300

    Versión Final 1 del Gestor --> No probado

--- gestion_clientes/gestor_clientes.py
+++ gestion_clientes/gestor_clientes.py
@@ -59,14 +59,51 @@ class GestorClientes:
 
         print("Cliente agregado correctamente.")
 
-    def listar_clientes(self):
+     def listar_clientes(self):
         """
-        Debería mostrar todos los clientes según su tipo
+        Muestra todos los clientes registrados en el sistema,
+        organizados por tipo.
         """
+
+        # Verificamos si hay al menos un cliente registrado
+        if all(len(lista) == 0 for lista in self._clientes.values()):
+            print("No hay clientes registrados.")
+            return
+
+        print("Listado de clientes")
+
+        # Poner Separador visual
+
+        # Recorremos el diccionario de clientes
+        for tipo, lista_clientes in self._clientes.items():
+
+            print(f"\nClientes tipo {tipo.capitalize()}:")
+
+        # Poner Separador visual
+
+
+            # Si no hay clientes de ese tipo, lo indicamos
+            if not lista_clientes:
+                print("No hay clientes de este tipo.")
+                continue
+
+            # Recorremos la lista de clientes del tipo actual
+            for cliente in lista_clientes:
+                print(cliente)
+
+    def eliminar_cliente(self, id_cliente):
         """
-        Verificar si hay al menos 1 cliente registrado
-        Que recorra el diccionario
-        Que elimine clientes
-        
+        Elimina un cliente del sistema según su ID.
+
+        Parámetro:
+        id_cliente: identificador del cliente a eliminar
         """
-        
+
+        # Recorremos todas las listas del diccionario---> Apoyo Italo
+        for lista_clientes in self._clientes.values():
+            for cliente in lista_clientes:
+                if cliente.id == id_cliente:
+                    lista_clientes.remove(cliente)
+                    print("Cliente eliminado correctamente.")
+                    return
+
