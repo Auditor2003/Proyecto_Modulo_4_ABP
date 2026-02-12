ee361e89b5b77f82bd5605375f7e4f5991deff24
Auditor2003 <diegomunozlasanta@gmail.com>
2026-02-12 20:50:53 -0300

    Revisar errores de indentación

--- clientes/cliente_premium.py
+++ clientes/cliente_premium.py
@@ -1,5 +1,4 @@
 """
-
 Tenemos a un cliente con beneficios especiales,
 como un descuento porcentual.
 """
@@ -11,17 +10,17 @@ from clientes.cliente import Cliente
 class ClientePremium(Cliente):
     """
     Clase ClientePremium.
-
     Hereda de Cliente y agrega un atributo adicional:
-    - descuento: porcentaje de descuento del cliente.
+    descuento: porcentaje de descuento del cliente.
     """
 
     def __init__(self, id_cliente, nombre, email, descuento, estado=True):
-        """
-        Constructor de ClientePremium.
 
+"""
+        Constructor de ClientePremium.
         Parámetros:
         - id_cliente: identificador único del cliente
         - nombre: nombre del cliente
         - email: correo electrónico
         - descuento: porcentaje de descuento (0 a 100)
+"""
